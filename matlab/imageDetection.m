87d132cbce24096d406789c161188b7def1a3b5b
cliu314 <clareliu@mit.edu>
2015-05-10 22:43:13 -0400

    updated matlab file

--- matlab/imageDetection.m
+++ matlab/imageDetection.m
@@ -3,4 +3,7 @@ image = imread(filename);
 I = rgb2gray(image);
 regions = detectMSERFeatures(I, 'ThresholdDelta', 3.0);
 numFeatures = regions.length;
+fileID = fopen('numRegions.txt', 'w');
+fprintf(fileID, '%d\n', numFeatures);
+fclose(fileID);
 end
