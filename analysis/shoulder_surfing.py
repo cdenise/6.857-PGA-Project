02c67be5f135e336fde62c6fea71c20d4902ebb5
Denise Che <cdenise@mit.edu>
2015-05-11 20:43:16 -0400

    image detection web widget

--- analysis/shoulder_surfing.py
+++ analysis/shoulder_surfing.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from firebase import firebase
+import math
 
 # Get results
 print "getting shoulder surfing results from 'https://6857-project-pga.firebaseio.com/'\n"
@@ -36,7 +37,7 @@ for entry in results.values():
                 gesture_output['radius'] = float(gestures[i][3])
             elif gesture_type == "line":
                 gesture_output['start'] = (float(gestures[i][1]), float(gestures[i][2]))
-                gesture_output['end'] = (float(gestures[i][1]), float(gestures[i][2]))
+                gesture_output['end'] = (float(gestures[i][3]), float(gestures[i][4]))
             else:
                 gesture_output['center'] = (float(gestures[i][1]), float(gestures[i][2]))
             if answer:
@@ -79,59 +80,199 @@ for entry in results.values():
 ##print
 
 ## saves results
-f1 = open('animals2.txt', 'w')
-f1.write('animals2 \n')
-for i in range(3):
-    f1.write('gesture ' + str(i + 1) + '\n')
-    f1.write('**************************************************\n')
-    f1.write('ANSWER\n')
-    j = animals2['answer'][i]
-    f1.write(j['type'])
-    if j['type'] == "circle":
-        f1.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
-    elif j['type'] == "tap":
-        f1.write('\t\t' + str(j['center']) + '\n')
-    else:
-        f1.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
-    f1.write('**************************************************\n')
-    gestures = animals2['gesture' + str(i + 1)]
-    for j in gestures:
-        f1.write(j['type'])
-        if j['type'] == "circle":
-            f1.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
-        elif j['type'] == "tap":
-            f1.write('\t\t' + str(j['center']) + '\n')
-        else:
-            f1.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
-    f1.write('\n')
-f1.close()
+##f1 = open('animals2.txt', 'w')
+##f1.write('animals2 \n')
+##for i in range(3):
+##    f1.write('gesture ' + str(i + 1) + '\n')
+##    f1.write('**************************************************\n')
+##    f1.write('ANSWER\n')
+##    j = animals2['answer'][i]
+##    f1.write(j['type'])
+##    if j['type'] == "circle":
+##        f1.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
+##    elif j['type'] == "tap":
+##        f1.write('\t\t' + str(j['center']) + '\n')
+##    else:
+##        f1.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
+##    f1.write('**************************************************\n')
+##    gestures = animals2['gesture' + str(i + 1)]
+##    for j in gestures:
+##        f1.write(j['type'])
+##        if j['type'] == "circle":
+##            f1.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
+##        elif j['type'] == "tap":
+##            f1.write('\t\t' + str(j['center']) + '\n')
+##        else:
+##            f1.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
+##    f1.write('\n')
+##f1.close()
+##
+##f2 = open('waldo2.txt', 'w')
+##f2.write('waldo2 \n')
+##for i in range(3):
+##    f2.write('gesture ' + str(i + 1) + '\n')
+##    f2.write('**************************************************\n')
+##    f2.write('ANSWER\n')
+##    j = waldo2['answer'][i]
+##    f2.write(j['type'])
+##    if j['type'] == "circle":
+##        f2.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
+##    elif j['type'] == "tap":
+##        f2.write('\t\t' + str(j['center']) + '\n')
+##    else:
+##        f2.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
+##    f2.write('**************************************************\n')
+##    gestures = waldo2['gesture' + str(i + 1)]
+##    for j in gestures:
+##        f2.write(j['type'])
+##        if j['type'] == "circle":
+##            f2.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
+##        elif j['type'] == "tap":
+##            f2.write('\t\t' + str(j['center']) + '\n')
+##        else:
+##            f2.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
+##    f2.write('\n')
+##f2.close()
 
-f2 = open('waldo2.txt', 'w')
-f2.write('waldo2 \n')
-for i in range(3):
-    f2.write('gesture ' + str(i + 1) + '\n')
-    f2.write('**************************************************\n')
-    f2.write('ANSWER\n')
-    j = waldo2['answer'][i]
-    f2.write(j['type'])
-    if j['type'] == "circle":
-        f2.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
-    elif j['type'] == "tap":
-        f2.write('\t\t' + str(j['center']) + '\n')
-    else:
-        f2.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
-    f2.write('**************************************************\n')
-    gestures = waldo2['gesture' + str(i + 1)]
-    for j in gestures:
-        f2.write(j['type'])
-        if j['type'] == "circle":
-            f2.write('\t\t' + str(j['center']) + '\t\t' + str(j['radius']) + '\n')
-        elif j['type'] == "tap":
-            f2.write('\t\t' + str(j['center']) + '\n')
-        else:
-            f2.write('\t\t' + str(j['start']) + '\t\t' + str(j['end']) + '\n')
-    f2.write('\n')
-f2.close()
+### Analysis for Animals2
+##print '**************************************************'
+##print 'Animals2'
+##print 'gesture 1'
+##print '**************************************************'
+##correct_type = animals2['answer'][1-1]['type']
+##print 'correct_type:', correct_type
+##x_c, y_c = animals2['answer'][1-1]['center']
+##correct_type_count = 0
+##total_error_c = 0
+##total_error_r = 0
+##for j in animals2['gesture1']:
+##    if j['type'] == correct_type:
+##        correct_type_count += 1
+##        x, y = j['center']
+##        total_error_c += math.sqrt((x-x_c)**2 + (y-y_c)**2)
+##        total_error_r += j['radius']
+##print 'total count:', len(animals2['gesture2'])
+##print 'correct_type_count:', correct_type_count
+##print 'correct_type_rate: ' + str(correct_type_count / float(len(animals2['gesture2'])) * 100) + "%"
+##print 'average_error (center):', total_error_c / correct_type_count
+##print 'average_error (radius):', total_error_r / correct_type_count
+##print
+##print
+##print '**************************************************'
+##print 'Animals2'
+##print 'gesture 2'
+##print '**************************************************'
+##correct_type = animals2['answer'][2-1]['type']
+##print 'correct_type:', correct_type
+##x_c, y_c = animals2['answer'][2-1]['center']
+##correct_type_count = 0
+##total_error = 0
+##for j in animals2['gesture2']:
+##    if j['type'] == correct_type:
+##        correct_type_count += 1
+##        x, y = j['center']
+##        total_error += math.sqrt((x-x_c)**2 + (y-y_c)**2)
+##print 'total count:', len(animals2['gesture2'])
+##print 'correct_type_count:', correct_type_count
+##print 'correct_type_rate: ' + str(correct_type_count / float(len(animals2['gesture2'])) * 100) + "%"
+##print 'average_error:', total_error / correct_type_count
+##print
+##print
+##print '**************************************************'
+##print 'Animals2'
+##print 'gesture 3'
+##print '**************************************************'
+##correct_type = animals2['answer'][3-1]['type']
+##print 'correct_type:', correct_type
+##x_c, y_c = animals2['answer'][3-1]['center']
+##correct_type_count = 0
+##total_error_c = 0
+##total_error_r = 0
+##for j in animals2['gesture3']:
+##    if j['type'] == correct_type:
+##        correct_type_count += 1
+##        x, y = j['center']
+##        total_error_c += math.sqrt((x-x_c)**2 + (y-y_c)**2)
+##        total_error_r += j['radius']
+##print 'total count:', len(animals2['gesture2'])
+##print 'correct_type_count:', correct_type_count
+##print 'correct_type_rate: ' + str(correct_type_count / float(len(animals2['gesture2'])) * 100) + "%"
+##print 'average_error (center):', total_error_c / correct_type_count
+##print 'average_error (radius):', total_error_r / correct_type_count
+##print
+##print
+
+# Analysis for Waldo2
+print '**************************************************'
+print 'Waldo2'
+print 'gesture 1'
+print '**************************************************'
+correct_type = waldo2['answer'][1-1]['type']
+print 'correct_type:', correct_type
+x_c, y_c = waldo2['answer'][1-1]['center']
+correct_type_count = 0
+total_error_c = 0
+##total_error_r = 0
+for j in waldo2['gesture1']:
+    if j['type'] == correct_type:
+        correct_type_count += 1
+        x, y = j['center']
+        total_error_c += math.sqrt((x-x_c)**2 + (y-y_c)**2)
+##        total_error_r += j['radius']
+print 'total count:', len(waldo2['gesture2'])
+print 'correct_type_count:', correct_type_count
+print 'correct_type_rate: ' + str(correct_type_count / float(len(waldo2['gesture2'])) * 100) + "%"
+print 'average_error (center):', total_error_c / correct_type_count
+##print 'average_error (radius):', total_error_r / correct_type_count
+print
+print
+print '**************************************************'
+print 'Waldo2'
+print 'gesture 2'
+print '**************************************************'
+correct_type = waldo2['answer'][2-1]['type']
+print 'correct_type:', correct_type
+x_s, y_s = waldo2['answer'][2-1]['start']
+x_e, y_e = waldo2['answer'][2-1]['end']
+correct_type_count = 0
+total_error_s = 0
+total_error_e = 0
+for j in waldo2['gesture2']:
+    if j['type'] == correct_type:
+        correct_type_count += 1
+        total_error_s += math.sqrt((j['start'][0] - x_s)**2 + (j['start'][1]-y_s)**2)
+        total_error_e += math.sqrt((j['end'][0] - x_e)**2 + (j['end'][1]-y_e)**2)
+print 'total count:', len(waldo2['gesture2'])
+print 'correct_type_count:', correct_type_count
+print 'correct_type_rate: ' + str(correct_type_count / float(len(waldo2['gesture2'])) * 100) + "%"
+print 'average_error (start):', total_error_s / correct_type_count
+print 'average_error (end):', total_error_e / correct_type_count
+print
+print
+print '**************************************************'
+print 'Waldo2'
+print 'gesture 3'
+print '**************************************************'
+correct_type = waldo2['answer'][3-1]['type']
+print 'correct_type:', correct_type
+x_c, y_c = waldo2['answer'][3-1]['center']
+correct_type_count = 0
+total_error_c = 0
+total_error_r = 0
+for j in waldo2['gesture3']:
+    if j['type'] == correct_type:
+        correct_type_count += 1
+        x, y = j['center']
+        total_error_c += math.sqrt((x-x_c)**2 + (y-y_c)**2)
+        total_error_r += j['radius']
+print 'total count:', len(waldo2['gesture2'])
+print 'correct_type_count:', correct_type_count
+print 'correct_type_rate: ' + str(correct_type_count / float(len(waldo2['gesture2'])) * 100) + "%"
+print 'average_error (center):', total_error_c / correct_type_count
+print 'average_error (radius):', total_error_r / correct_type_count
+print
+print
+
 
 
 
