412914553d52257d2ad09825753e484950e44125
cliu314 <clareliu@mit.edu>
2015-05-11 23:55:24 -0400

    compiled matlab

--- matlab/imageDetection.m
+++ matlab/imageDetection.m
@@ -3,5 +3,5 @@ image = imread(filename);
 I = rgb2gray(image);
 regions = detectMSERFeatures(I, 'ThresholdDelta', 3.0);
 numFeatures = regions.length;
-system(strcat('python pushData.py ', num2str(numFeatures)));
+system(char(strcat({'python pushData.py '}, num2str(numFeatures))));
 end
