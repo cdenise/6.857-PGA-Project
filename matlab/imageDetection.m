d4bc51ff4ca06eba5987b6fc75fd6f500aced920
Denise Che <cdenise@mit.edu>
2015-05-11 23:05:18 -0400

    call python script to push numFeatures to firebase

--- matlab/imageDetection.m
+++ matlab/imageDetection.m
@@ -3,7 +3,5 @@ image = imread(filename);
 I = rgb2gray(image);
 regions = detectMSERFeatures(I, 'ThresholdDelta', 3.0);
 numFeatures = regions.length;
-fileID = fopen('numRegions.txt', 'w');
-fprintf(fileID, '%d\n', numFeatures);
-fclose(fileID);
+system(strcat('python pushData.py ', num2str(numFeatures)));
 end
